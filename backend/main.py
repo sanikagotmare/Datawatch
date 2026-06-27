54c0159e65628b70acf25ebecabe1ab54239be2e
Sanika Gotmare <gotmaresanika@gmail.com>
2026-06-27 10:05:01 +0530

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -43,12 +43,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-   allow_origins=[
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://datawatch-frontend-eta.vercel.app",
-    "https://datawatch-frontend-ngin7244r-sanikagotmares-projects.vercel.app",
-],
+    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
